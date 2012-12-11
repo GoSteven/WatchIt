3761d63c72a8eec259054e3a3806d43921ac7322
Steven You <yousilin@gmail.com>
2012-12-12 00:14:12 +1100

    try PyMongo

--- wsgi/watchit.py
+++ wsgi/watchit.py
@@ -5,7 +5,7 @@ from bson import json_util
 import os
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL'] + os.environ['OPENSHIFT_APP_NAME'] + '?auto_start_request=True'
+app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL'] + os.environ['OPENSHIFT_APP_NAME'] + '?auto_start_request=true'
 mongo = PyMongo(app)
 
 @app.route('/')
