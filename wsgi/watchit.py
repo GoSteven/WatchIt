e4ae546e64d2ce1863280c6adcb2eff3d4162a95
Steven You <yousilin@gmail.com>
2012-12-11 23:41:19 +1100

    try PyMongo

--- wsgi/watchit.py
+++ wsgi/watchit.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask.ext.pymongo import PyMongo
+from bson.json_util import dumps
+import os
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL'] + os.environ['OPENSHIFT_APP_NAME']
 mongo = PyMongo(app)
 
 @app.route('/')
 def home_page():
-    return 'hello steven'
+    return dumps(mongo.db.openshift.find_one())
 
 
 
